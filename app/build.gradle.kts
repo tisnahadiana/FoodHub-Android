378685be8fd6f282bec67c7b712ad458ea28a25f
Deeromptech <tisnahadiana@gmail.com>
2024-12-05 14:47:42 +0700

    feat(App): adding splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.deeromptech.foodhub_android"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.deeromptech.foodhub_android"
@@ -56,4 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.core.splashscreen)
 }
--- app/src/main/java/com/deeromptech/foodhub_android/MainActivity.kt
+++ app/src/main/java/com/deeromptech/foodhub_android/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.deeromptech.foodhub_android
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +14,54 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.deeromptech.foodhub_android.ui.theme.FoodHubAndroidTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    var showSplashScreen = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                showSplashScreen
+            }
+            setOnExitAnimationListener { screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.5f,
+                    0f
+                )
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.5f,
+                    0f
+                )
+                zoomX.duration = 500
+                zoomY.duration = 500
+                zoomX.interpolator = OvershootInterpolator()
+                zoomY.interpolator = OvershootInterpolator()
+                zoomX.doOnEnd {
+                    screen.remove()
+                }
+                zoomY.doOnEnd {
+                    screen.remove()
+                }
+                zoomY.start()
+                zoomX.start()
+            }
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             FoodHubAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -26,6 +71,10 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(3000)
+            showSplashScreen = false
         }
     }
 }
